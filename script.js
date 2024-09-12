70a557449118bd37ce5b63da45116d5a8fb3a493
David Hay <yomanhay@comcast.net>
2024-09-12 13:18:34 -0400

    final product

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const container = document.querySelector(".container");
 const toggle = document.querySelector(".toggle");
+const info = document.querySelector(".toggle-info");
 
 toggle.addEventListener("click", () => {
-    container.classList.toggle("dark")
+    container.classList.toggle("dark") ? 
+    (toggle.firstElementChild.className = "far fa-moon", info.innerHTML = "Toggle Light Mode")
+    :(toggle.firstElementChild.className = "far fa-sun", info.innerHTML = "Toggle Dark Mode")
+})
+info.addEventListener("click", () => {
+    container.classList.toggle("dark") ? 
+    (toggle.firstElementChild.className = "far fa-moon", info.innerHTML = "Toggle Light Mode")
+    :(toggle.firstElementChild.className = "far fa-sun", info.innerHTML = "Toggle Dark Mode")
 })
